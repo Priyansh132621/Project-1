0e0dff93168a98d737d302ec77910245f0daa4e7
Prince <priyanshmankotia26@gmail.com>
2024-04-26 02:27:15 +0530

    Projext-1

---
+++ src/config.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const connect = mongoose.connect("mongodb://localhost:27017/Login");
+connect.then(()=>{
+    console.log("Database connected")
+})
+.catch(()=>{
+    console.log("Database not connected ")
+});
+
+
+
+//create a schema
+
+const LoginSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+//create a collection
+
+const collection = new mongoose.model('users', LoginSchema);
+
+module.exports = collection;
